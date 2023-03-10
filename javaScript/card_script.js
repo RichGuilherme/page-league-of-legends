902a0affee398fbfa3a2f787e7b55c13f24eb8cc
Richard Guilherme <richard.guilhermeAS@hotmail.com>
2023-03-10 00:17:19 -0300

    Update card_script.js

--- javaScript/card_script.js
+++ javaScript/card_script.js
@@ -1,249 +1,118 @@
-const img1 = document.querySelector("#card_img1")
-const img2 = document.querySelector("#card_img2")
-const img3 = document.querySelector("#card_img3")
-const img4 = document.querySelector("#card_img4")
-const img5 = document.querySelector("#card_img5")
-
-const magos = ["Ahri", "Lux", "Zoe", "Leblanc", "Syndra"]
-const lutadores = ["Yasuo", "Fiora", "Irelia", "Ekko", "LeeSin"]  // Cada nome representa imagem de um campeão no arquivo
-const assassinos = ["Zed", "Akali", "Shaco", "Talon", "Khazix"]
-const atiradores = ["Caitlyn", "Jinx", "Ashe", "Ezreal", "Jhin"]
-const tanques = ["Darius", "Braum", "Alistar", "Leona", "Aatrox"]
-
-let index = 1
-function buttonProxImagem () {
-
-
-switch(index){
-    case 0:
-        index++
-        break;
-    case 1:
-        img1.style.backgroundImage = `url(img/campeõesTelaMenu/magos/${magos[1]}.jpg)`
-        document.querySelector("#trocarCampeao1").innerHTML = "Lux"
-        document.querySelector("#trocarDescricao1").innerHTML = "A dama da luz"
-
-        img2.style.backgroundImage = `url(img/campeõesTelaMenu/lutadores/${lutadores[1]}.jpg)`
-        document.querySelector("#trocarCampeao2").innerHTML = "Fiora"
-        document.querySelector("#trocarDescricao2").innerHTML = "A grande duelista"
-
-        img3.style.backgroundImage = `url(img/campeõesTelaMenu/assassinos/${assassinos[1]}.jpg)`
-        document.querySelector("#trocarCampeao3").innerHTML = "Akali"
-        document.querySelector("#trocarDescricao3").innerHTML = "A assassina renegada"
-
-        img4.style.backgroundImage = `url(img/campeõesTelaMenu/atiradores/${atiradores[1]}.jpg)`
-        document.querySelector("#trocarCampeao4").innerHTML = "Jinx"
-        document.querySelector("#trocarDescricao4").innerHTML = "O gatilho desenfreado"
-
-        img5.style.backgroundImage = `url(img/campeõesTelaMenu/tanks/${tanques[1]}.jpg)`
-        document.querySelector("#trocarCampeao5").innerHTML = "Braum"
-        document.querySelector("#trocarDescricao5").innerHTML = "O coração de Freljord"
-
-        index++
-        break;
-    case 2:
-        img1.style.backgroundImage = `url(img/campeõesTelaMenu/magos/${magos[2]}.jpg)`
-        document.querySelector("#trocarCampeao1").innerHTML = "Zoe"
-        document.querySelector("#trocarDescricao1").innerHTML = "O aspecto do crepúsculo"
-
-        img2.style.backgroundImage = `url(img/campeõesTelaMenu/lutadores/${lutadores[2]}.jpg)`
-        document.querySelector("#trocarCampeao2").innerHTML = "Irelia"
-        document.querySelector("#trocarDescricao2").innerHTML = "A dançarina das lâminas"
-
-        img3.style.backgroundImage = `url(img/campeõesTelaMenu/assassinos/${assassinos[2]}.jpg)`
-        document.querySelector("#trocarCampeao3").innerHTML = "Shaco"
-        document.querySelector("#trocarDescricao3").innerHTML = "O bufão demoníaco"
-
-        img4.style.backgroundImage = `url(img/campeõesTelaMenu/atiradores/${atiradores[2]}.jpg)`
-        document.querySelector("#trocarCampeao4").innerHTML = "Ashe"
-        document.querySelector("#trocarDescricao4").innerHTML = "A arqueira do gelo"
-
-        img5.style.backgroundImage = `url(img/campeõesTelaMenu/tanks/${tanques[2]}.jpg)`
-        document.querySelector("#trocarCampeao5").innerHTML = "alista"
-        document.querySelector("#trocarDescricao5").innerHTML = "O minotauro"
-
-        index++
-        break;
-    case 3:
-        img1.style.backgroundImage = `url(img/campeõesTelaMenu/magos/${magos[3]}.jpg)`
-        document.querySelector("#trocarCampeao1").innerHTML = "Leblanc"
-        document.querySelector("#trocarDescricao1").innerHTML = "A farsante"
-
-        img2.style.backgroundImage = `url(img/campeõesTelaMenu/lutadores/${lutadores[3]}.jpg)`
-        document.querySelector("#trocarCampeao2").innerHTML = "Ekko"
-        document.querySelector("#trocarDescricao2").innerHTML = "O rapaz que estilhaçou o tempo"
-
-        img3.style.backgroundImage = `url(img/campeõesTelaMenu/assassinos/${assassinos[3]}.jpg)`
-        document.querySelector("#trocarCampeao3").innerHTML = "Talon"
-        document.querySelector("#trocarDescricao3").innerHTML = "A sombra da lâmina"
-
-        img4.style.backgroundImage = `url(img/campeõesTelaMenu/atiradores/${atiradores[3]}.jpg)`
-        document.querySelector("#trocarCampeao4").innerHTML = "Ezreal"
-        document.querySelector("#trocarDescricao4").innerHTML = "O explorador pródigo"
-
-        img5.style.backgroundImage = `url(img/campeõesTelaMenu/tanks/${tanques[3]}.jpg)`
-        document.querySelector("#trocarCampeao5").innerHTML = "Leona"
-        document.querySelector("#trocarDescricao5").innerHTML = "A alvorada radiante"
-
-        index++
-        break;
-    case 4:
-        img1.style.backgroundImage = `url(img/campeõesTelaMenu/magos/${magos[4]}.jpg)`
-        document.querySelector("#trocarCampeao1").innerHTML = "Syndra"
-        document.querySelector("#trocarDescricao1").innerHTML = "A soberana sombria"
-        
-        img2.style.backgroundImage = `url(img/campeõesTelaMenu/lutadores/${lutadores[4]}.jpg)`
-        document.querySelector("#trocarCampeao2").innerHTML = "Lee Sin"
-        document.querySelector("#trocarDescricao2").innerHTML = "O monge cedo"
-
-        img3.style.backgroundImage = `url(img/campeõesTelaMenu/assassinos/${assassinos[4]}.jpg)`
-        document.querySelector("#trocarCampeao3").innerHTML = "Kha'zix"
-        document.querySelector("#trocarDescricao3").innerHTML = "O ceifador do vazio"
-
-        img4.style.backgroundImage = `url(img/campeõesTelaMenu/atiradores/${atiradores[4]}.jpg)`
-        document.querySelector("#trocarCampeao4").innerHTML = "Jhin"
-        document.querySelector("#trocarDescricao4").innerHTML = "O virtuoso"
-
-        img5.style.backgroundImage = `url(img/campeõesTelaMenu/tanks/${tanques[4]}.jpg)`
-        document.querySelector("#trocarCampeao5").innerHTML = "Aatrox"
-        document.querySelector("#trocarDescricao5").innerHTML = "A espada darkin"
-        
-        index++
-        break;
-}
-}
-
-function buttonbackImagem() {
-if(index == 1){
-    indice = 0
-}else{
-    indice = index - 2
-    index--
-}
-
-
-
-    switch(indice){
-        case 0:
-            img1.style.backgroundImage = `url(img/campeõesTelaMenu/magos/${magos[0]}.jpg)`
-            document.querySelector("#trocarCampeao1").innerHTML = "Ahri"
-            document.querySelector("#trocarDescricao1").innerHTML = "Raposa de nove caudas"
-    
-            img2.style.backgroundImage = `url(img/campeõesTelaMenu/lutadores/${lutadores[0]}.jpg)`
-            document.querySelector("#trocarCampeao2").innerHTML = "Yasuo"
-            document.querySelector("#trocarDescricao2").innerHTML = "O imperdoável"
-    
-            img3.style.backgroundImage = `url(img/campeõesTelaMenu/assassinos/${assassinos[0]}.jpg)`
-            document.querySelector("#trocarCampeao3").innerHTML = "Zed"
-            document.querySelector("#trocarDescricao3").innerHTML = "O mestre das sombras"
-    
-            img4.style.backgroundImage = `url(img/campeõesTelaMenu/atiradores/${atiradores[0]}.jpg)`
-            document.querySelector("#trocarCampeao4").innerHTML = "Caitlyn"
-            document.querySelector("#trocarDescricao4").innerHTML = "A xerife de piltover"
-    
-            img5.style.backgroundImage = `url(img/campeõesTelaMenu/tanks/${tanques[0]}.jpg)`
-            document.querySelector("#trocarCampeao5").innerHTML = "Darius"
-            document.querySelector("#trocarDescricao5").innerHTML = "A mão de noxus"
-
-            break;
-        case 1:
-            img1.style.backgroundImage = `url(img/campeõesTelaMenu/magos/${magos[1]}.jpg)`
-            document.querySelector("#trocarCampeao1").innerHTML = "Lux"
-            document.querySelector("#trocarDescricao1").innerHTML = "A dama da luz"
-    
-            img2.style.backgroundImage = `url(img/campeõesTelaMenu/lutadores/${lutadores[1]}.jpg)`
-            document.querySelector("#trocarCampeao2").innerHTML = "Fiora"
-            document.querySelector("#trocarDescricao2").innerHTML = "A grande duelista"
-    
-            img3.style.backgroundImage = `url(img/campeõesTelaMenu/assassinos/${assassinos[1]}.jpg)`
-            document.querySelector("#trocarCampeao3").innerHTML = "Akali"
-            document.querySelector("#trocarDescricao3").innerHTML = "A assassina renegada"
-    
-            img4.style.backgroundImage = `url(img/campeõesTelaMenu/atiradores/${atiradores[1]}.jpg)`
-            document.querySelector("#trocarCampeao4").innerHTML = "Jinx"
-            document.querySelector("#trocarDescricao4").innerHTML = "O gatilho desenfreado"
-    
-            img5.style.backgroundImage = `url(img/campeõesTelaMenu/tanks/${tanques[1]}.jpg)`
-            document.querySelector("#trocarCampeao5").innerHTML = "Braum"
-            document.querySelector("#trocarDescricao5").innerHTML = "O coração de Freljord"
-            
-
-            indice--
-            break;
-        case 2:
-            img1.style.backgroundImage = `url(img/campeõesTelaMenu/magos/${magos[2]}.jpg)`
-            document.querySelector("#trocarCampeao1").innerHTML = "Zoe"
-            document.querySelector("#trocarDescricao1").innerHTML = "O aspecto do crepúsculo"
-    
-            img2.style.backgroundImage = `url(img/campeõesTelaMenu/lutadores/${lutadores[2]}.jpg)`
-            document.querySelector("#trocarCampeao2").innerHTML = "Irelia"
-            document.querySelector("#trocarDescricao2").innerHTML = "A dançarina das lâminas"
-    
-            img3.style.backgroundImage = `url(img/campeõesTelaMenu/assassinos/${assassinos[2]}.jpg)`
-            document.querySelector("#trocarCampeao3").innerHTML = "Shaco"
-            document.querySelector("#trocarDescricao3").innerHTML = "O bufão demoníaco"
-    
-            img4.style.backgroundImage = `url(img/campeõesTelaMenu/atiradores/${atiradores[2]}.jpg)`
-            document.querySelector("#trocarCampeao4").innerHTML = "Ashe"
-            document.querySelector("#trocarDescricao4").innerHTML = "A arqueira do gelo"
-    
-            img5.style.backgroundImage = `url(img/campeõesTelaMenu/tanks/${tanques[2]}.jpg)`
-            document.querySelector("#trocarCampeao5").innerHTML = "alista"
-            document.querySelector("#trocarDescricao5").innerHTML = "O minotauro"
-             
-            indice--
-            break;
-        case 3:
-            img1.style.backgroundImage = `url(img/campeõesTelaMenu/magos/${magos[3]}.jpg)`
-            document.querySelector("#trocarCampeao1").innerHTML = "Leblanc"
-            document.querySelector("#trocarDescricao1").innerHTML = "A farsante"
-    
-            img2.style.backgroundImage = `url(img/campeõesTelaMenu/lutadores/${lutadores[3]}.jpg)`
-            document.querySelector("#trocarCampeao2").innerHTML = "Ekko"
-            document.querySelector("#trocarDescricao2").innerHTML = "O rapaz que estilhaçou o tempo"
-    
-            img3.style.backgroundImage = `url(img/campeõesTelaMenu/assassinos/${assassinos[3]}.jpg)`
-            document.querySelector("#trocarCampeao3").innerHTML = "Talon"
-            document.querySelector("#trocarDescricao3").innerHTML = "A sombra da lâmina"
-    
-            img4.style.backgroundImage = `url(img/campeõesTelaMenu/atiradores/${atiradores[3]}.jpg)`
-            document.querySelector("#trocarCampeao4").innerHTML = "Ezreal"
-            document.querySelector("#trocarDescricao4").innerHTML = "O explorador pródigo"
-    
-            img5.style.backgroundImage = `url(img/campeõesTelaMenu/tanks/${tanques[3]}.jpg)`
-            document.querySelector("#trocarCampeao5").innerHTML = "Leona"
-            document.querySelector("#trocarDescricao5").innerHTML = "A alvorada radiante"
-    
-
-            indice--
-            break;
-        case 4:
-            img1.style.backgroundImage = `url(img/campeõesTelaMenu/magos/${magos[4]}.jpg)`
-            document.querySelector("#trocarCampeao1").innerHTML = "Syndra"
-            document.querySelector("#trocarDescricao1").innerHTML = "A soberana sombria"
-            
-            img2.style.backgroundImage = `url(img/campeõesTelaMenu/lutadores/${lutadores[4]}.jpg)`
-            document.querySelector("#trocarCampeao2").innerHTML = "Lee Sin"
-            document.querySelector("#trocarDescricao2").innerHTML = "O monge cedo"
-    
-            img3.style.backgroundImage = `url(img/campeõesTelaMenu/assassinos/${assassinos[4]}.jpg)`
-            document.querySelector("#trocarCampeao3").innerHTML = "Kha'zix"
-            document.querySelector("#trocarDescricao3").innerHTML = "O ceifador do vazio"
-    
-            img4.style.backgroundImage = `url(img/campeõesTelaMenu/atiradores/${atiradores[4]}.jpg)`
-            document.querySelector("#trocarCampeao4").innerHTML = "Jhin"
-            document.querySelector("#trocarDescricao4").innerHTML = "O virtuoso"
-    
-            img5.style.backgroundImage = `url(img/campeõesTelaMenu/tanks/${tanques[4]}.jpg)`
-            document.querySelector("#trocarCampeao5").innerHTML = "Aatrox"
-            document.querySelector("#trocarDescricao5").innerHTML = "A espada darkin"
-    
+const arraysDeExibição = [[ /*primeira exibição*/
+    { campeao: 'Ahri',
+      classe: 'magos',
+      descricao: 'Raposa de nove cauda'},
+    {campeao: 'Yasuo',
+      classe: 'lutadores',
+      descricao: 'O imperdoável'},
+    {campeao: 'Zed',
+     classe: 'assassinos',
+      descricao: 'O mestre das sombras'},
+    {campeao: 'Caitlyn',
+    classe: 'atiradores',
+     descricao: 'A xerife de piltover'},
+    {campeao: 'Darius',
+     classe: 'tanks',
+     descricao: 'A mão de noxus'}
+ ],[  /*Segunda exibição*/
+  { campeao: 'Lux',
+    classe: 'magos',
+    descricao: 'A dama da luz'},
+  {campeao: 'Fiora',
+   classe: 'lutadores',
+    descricao: 'A grande duelista'},
+  {campeao: 'Akali',
+   classe: 'assassinos',
+    descricao: 's A assassina renegada'},
+  {campeao: 'Jinx',
+   classe: 'atiradores',
+   descricao: 'O gatilho desenfreado'},
+  {campeao: 'Braum',
+   classe: 'tanks',
+   descricao: 'O coração de freljord'}
+],[ /*teceira exibição  */
+  { campeao: 'Zoe',
+    classe: 'magos',
+    descricao: 'O aspecto do crepúsculo '},
+  {campeao: 'Irelia',
+    classe: 'lutadores',
+    descricao: 'A dançarina das lâminas'},
+  {campeao: 'Shaco',
+    classe: 'assassinos',
+    descricao: 'O bufão demoníaco'},
+  {campeao: 'Ashe',
+   classe: 'atiradores',
+   descricao: 'A arqueira de gelo'},
+  {campeao: 'Alistar',
+   classe: 'tanks',
+   descricao: 'O minotauro'}
+],[ /*Quarta exibição */
+  { campeao: 'Leblanc',
+    classe: 'magos',
+    descricao: 'A farsante'},
+  {campeao: 'Ekko',
+    classe: 'lutadores',
+    descricao: 'O rapaz que estilhaçou o tempo'},
+  {campeao: 'Talon',
+    classe: 'assassinos',
+    descricao: 'A sobmra da lâmina'},
+  {campeao: 'Ezreal',
+    classe: 'atiradores',
+   descricao: 'O explorador pródigo'},
+  {campeao: 'Leona',
+   classe: 'tanks',
+   descricao: 'A alvorada radiante'}
+],[ /*quinta exibição */
+  { campeao: 'Syndra',
+    classe: 'magos',
+    descricao: 'A soberana sombria'},
+  {campeao: 'Lee Sin',
+    classe: 'lutadores',
+    descricao: 'O monge cego'},
+  {campeao: "Kha'zix",
+    classe: 'assassinos',
+    descricao: 'O ceifador do vazio'},
+  {campeao: 'Jhin',
+    classe: 'atiradores',
+   descricao: 'O virtuoso'},
+  {campeao: 'Aatrox',
+    classe: 'tanks',
+   descricao: 'A espada darkin'}
+]
+]
  
-            indice--
-            break;
+let index = 1
+
+const getExebição = (exebicao) => {
+
+  const getArray = arraysDeExibição[exebicao]
+
+  getArray.forEach((value, indexValue = 0) => {
+    const getCampeao =  value.campeao
+    const getDescricao = value.descricao   
+    const getClass = value.classe
+   
+
+    const img =  document.querySelector(`#card_img${indexValue}`) 
+    img.style.backgroundImage = `url(img/campeõesTelaMenu/${getClass}/${getCampeao}.jpg)`
     
-    }
+    
+     document.querySelector(`#trocarCampeao${indexValue}`).innerHTML = getCampeao
+     document.querySelector(`#trocarDescricao${indexValue}`).innerHTML = getDescricao
+    
+  })
 }
+
+function buttonProxImagem () {
+  getExebição(index)
+  index++
+}
+
+function buttonBackImagem() {
+   index === 1 ? index : index-- 
+   getExebição(index - 1) 
+}
+
 
 
  
